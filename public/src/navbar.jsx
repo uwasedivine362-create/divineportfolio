982658b2ea2a02394d83271a9a97cc11403c9764
uwasedivine362@gmail.com <uwasedivine362@gmail.com>
2026-03-19 10:24:45 +0200

    added navbar section

---
+++ public/src/navbar.jsx
@@ -0,0 +1,24 @@
+function Navbar(){
+
+return(
+
+<nav className="nav">
+
+<h2>Divine Uwase</h2>
+
+<ul>
+<li><a href="#about">About</a></li>
+<li><a href="#education">Education</a></li>
+<li><a href="#experience">Experience</a></li>
+<li><a href="#skills">Skills</a></li>
+<li><a href="#projects">Projects</a></li>
+<li><a href="#contact">Contact</a></li>
+</ul>
+
+</nav>
+
+)
+
+}
+
+export default Navbar;
