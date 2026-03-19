3492e1abaf46e0d99cc4347463ebca2d48c7e190
uwasedivine362@gmail.com <uwasedivine362@gmail.com>
2026-03-19 10:26:19 +0200

    added profile section

---
+++ public/src/profile.jsx
@@ -0,0 +1,25 @@
+function Profile(){
+
+return(
+
+<section className="profile">
+
+<img src="/divinepoto.jpg" alt="Divine Uwase" className="divinepoto-img"/>
+
+<h1>Divine Uwase</h1>
+
+<p>Accounting & IT Support Professional</p>
+
+<p>Kigali – Rwanda</p>
+
+<a href="/Divine_Uwase_CV.pdf" download className="cv-btn">
+Download CV
+</a>
+
+</section>
+
+)
+
+}
+
+export default Profile;
